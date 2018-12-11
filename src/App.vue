e9a0e7d6927812c8991c9cff1102ff100fa97dd0
pushkov <Pushkov@oe-it.ru>
2018-12-11 12:11:24 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>{{msg}}</h1>
+    <a class="waves-effect waves-light btn">button</a>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
